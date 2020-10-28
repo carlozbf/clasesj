8b6762b8ac53c7185212d893460eadd47048d903
carloz <carloz.bohorquez@gmail.com>
2020-10-27 21:36:26 -0500

    subiendo un nuevo archivo.

---
+++ codigo/otro/Probar.java
@@ -0,0 +1,7 @@
+public class Probar{
+
+	public static void main (String args[]){
+		System.out.println("esta es una prueba!. from Matebook");
+		System.out.println("this is a test, for git");
+	}
+}
